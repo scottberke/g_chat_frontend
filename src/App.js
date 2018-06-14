b73603ba0bf52c39df28afc1991fe1c0b7d27019
Scott B <scottberke@gmail.com>
2018-06-13 22:43:11 -0600

    Update app.js and add handle login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
 import { Window } from './components/Window'
+import { Login } from './components/Login'
 import './App.css';
 import Cable from 'actioncable';
 import { Bootstrap, Grid, Row, Col } from 'react-bootstrap';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      access_token: ''
+    };
+  }
+
   render() {
-    return (
-      <Window />
-    );
+
+    if (this.state.isLoggedIn) {
+      return (
+        <Window accessToken={ this.state.access_token } />
+      );
+    } else {
+      return (
+        <div>
+        { this.renderLogin() }
+        </div>
+      );
+    }
+
+  }
+
+  renderLogin() {
+    var response = ''
+    return <Login handleLogin={ (response) => this.handleLogin(response) } />;
+  }
+
+  handleLogin(response) {
+      if (response.access_token) {
+        this.setState({
+          access_token: response.access_token,
+          isLoggedIn: true
+        });
+      } else {
+        this.renderLogin
+      }
   }
 }
 
